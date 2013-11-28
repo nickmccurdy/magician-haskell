7b62c00057ed6001ef77438c1403420c756fb220
Nicolas McCurdy <thenickperson@gmail.com>
2013-11-28 15:08:18 -0500

    Add Magician.List.none

--- src/Magician/List.hs
+++ src/Magician/List.hs
@@ -4,3 +4,8 @@ module Magician.List where
 -- backward). The list must be finite.
 palindrome :: Eq a => [a] -> Bool
 palindrome xs = xs == reverse xs
+
+-- |The opposite of any (in Prelude). Returns true iff a function returns false
+-- for every item in a list.
+none :: (a -> Bool) -> [a] -> Bool
+none f xs = not $ any f xs
