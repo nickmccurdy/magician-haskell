c9e1bcf4cc634ec503de2b913ac3f12e78ef6bd0
Nicolas McCurdy <thenickperson@gmail.com>
2013-11-28 06:00:16 -0500

    Use a better module/directory structure for the source code

--- src/Magician/List.hs
+++ src/Magician/List.hs
@@ -1,3 +1,5 @@
+module Magician.List where
+
 -- |Returns true if a list is a palindrome (meaning it is the same forward and
 -- backward). The list must be finite.
 palindrome :: Eq a => [a] -> Bool
---
+++ src/Main.hs
@@ -0,0 +1,1 @@
+import Magician.List
